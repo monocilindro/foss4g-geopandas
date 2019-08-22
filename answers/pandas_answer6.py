5a02990e62589d7e987a5e49c974476380d501ac
YaminiRao <32708814+YaminiRao@users.noreply.github.com>
2019-08-22 16:12:39 +0100

    Update pandas_answer6.py

--- answers/pandas_answer6.py
+++ answers/pandas_answer6.py
@@ -1,1 +1,3 @@
-sns.barplot(x="Median_House_Price,_2015", y="Area_name", data=boroughs,ax=ax);
+[fig, ax] = plt.subplots(1, figsize=(7,7))
+sns.barplot(x="Median_House_Price,_2015", y="Area_name", data=boroughs, ax=ax);
+
