ec20d12d44660aebf46f05938ce59a294b1fa97b
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2019-08-23 12:35:18 +0100

    Update pandas_answer5.py

--- answers/pandas_answer5.py
+++ answers/pandas_answer5.py
@@ -1,3 +1,4 @@
-boroughs['Gross_Annual_Pay_-_Female_(2016)'].plot.hist(bins=15,figsize=(10,5));
-boroughs['Gross_Annual_Pay_-_Male_(2016)'].plot.hist(bins=15,figsize=(10,5));
+ax = boroughs['Gross_Annual_Pay_-_Female_(2016)'].plot.hist(bins=15,figsize=(10,5),alpha=0.5);
+ax = boroughs['Gross_Annual_Pay_-_Male_(2016)'].plot.hist(bins=15,figsize=(10,5),alpha=0.5);
+ax.legend(['female','male']);
 
