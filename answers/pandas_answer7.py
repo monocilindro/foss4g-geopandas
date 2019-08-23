e493feaf2a8bb9591cc8077b39023af0a6b75ffc
YaminiRao <32708814+YaminiRao@users.noreply.github.com>
2019-08-23 15:14:08 +0100

    Update pandas_answer7.py

--- answers/pandas_answer7.py
+++ answers/pandas_answer7.py
@@ -1,2 +1,2 @@
 [fig, ax] = plt.subplots(1, figsize=(7,7))
-ax=sns.scatterplot(y='Area_name', x='%_of_area_that_is_Greenspace,_2005', data=boroughs,ax=ax);
+ax=sns.scatterplot(y='Median_House_Price,_2015', x='%_of_area_that_is_Greenspace,_2005', data=boroughs,ax=ax);
