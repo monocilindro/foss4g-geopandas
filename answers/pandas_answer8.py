ffde989be84c30f148afae329c24db918f34e8a7
YaminiRao <32708814+YaminiRao@users.noreply.github.com>
2019-08-23 15:15:58 +0100

    Update pandas_answer8.py

--- answers/pandas_answer8.py
+++ answers/pandas_answer8.py
@@ -1,1 +1,1 @@
-sns.catplot(x="Female_employment_rate_(2015)", y="Largest_migrant_population_arrived_during_2015/16", kind="box", data=boroughs);
+sns.catplot(x="Youth_Unemployment_(claimant)_rate_18-24_(Dec-15)", y="Largest_migrant_population_arrived_during_2015/16", kind="box", data=boroughs);
